c3efc0ca038c823cb61ecbc8b24bd811031f61af
SusejR <rodriguezbsusej@gmail.com>
2020-05-30 18:49:48 -0400

    iniciando vista home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,6 +4,11 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
+            <div className='us'>
+                <h2>Nosotros</h2>
+                <p>Conocenos, cual es nuestra visión, misión, como funciona nuestra app y como puedes aportar tu granito de arena</p>
+                <button>Conocer</button>
+            </div>
             
         </div>
     )
