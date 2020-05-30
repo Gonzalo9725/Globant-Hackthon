58a596090ffe52ca4dff1b83cf7d3a221c6c3d54
catalina-alzamora <alzamoracatalina@gmail.com>
2020-05-30 19:26:31 -0400

    starting CharityForm

--- src/Components/CharityForm.jsx
+++ src/Components/CharityForm.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CharityForm.css'
+import useInputState from "./useinputState"
+import TextField from "@material-ui/core/TextField"
 
 const CharityForm = () => {
+    const [data, setData] = useState({
+        Name = '',
+        RUT = ''
+    });
     return (
         <div>
-            
+            <Form className="">
+                <TextField
+                    placeHolder="Ingrese Nombre"
+                    name="Name"
+                    onChange="handleInputChange"
+                />
+                <TextField
+                    placeHolder="Ingrese RUT"
+                    name="RUT"
+                    onChange="handleInputChange"
+                />
+                <button>Enviar</button> />
+            </Form>
         </div>
     )
 }
