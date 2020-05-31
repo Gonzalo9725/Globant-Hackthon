17e06523d806fa1c363fb71e851013010244c94d
SusejR <rodriguezbsusej@gmail.com>
2020-05-31 13:38:32 -0400

    trabajando en vista mydonation

--- src/Components/MyDonations.jsx
+++ src/Components/MyDonations.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MyDonations.css'
-import { firebase } from '../firebase-config'
+import { db } from '../firebase-config'
 import FoodCard from './Widgets/FoodCard'
+import NavBar from './Widgets/NavBar'
 
 const MyDonations = () => {
-        
+
+    const [myDonations, setMyDonations] = useState(null)
+
+    useEffect(() => {
+        const foodDonations = db.collection('food').orderBy('time', 'desc');
+        foodDonations.onSnapshot((querySnapshot) => {
+            const food = [];
+    
+            querySnapshot.forEach(doc => {
+              const infoFood = 
+                  { dataOrder: doc.data(),
+                    id: doc.id }
+              food.push(infoFood);
+            });
+
+            setMyDonations(food)
+            console.log(food)
+        });
+    }, [])
+
+    const convertDay = (date) => {
+        const myDate = new Date( date *1000);
+        return myDate.toLocaleString().slice(0, 10);
+    }
+
     return (
-        <div>
-            <FoodCard/>
+        <div className='myDonations'>
+            <NavBar />
+            <h1 className='titleFoodSection'>Mis Donaciones:</h1>
+            <div className='myDonationsDiv'>
+            { myDonations &&
+            myDonations.map(food => {
+                return(
+                    <FoodCard
+                    key={food.id}
+                    title={food.dataOrder.title}
+                    qty={food.dataOrder.quantity}
+                    exp= {convertDay(food.dataOrder.expiration.seconds)}
+                    name= {food.dataOrder.name}
+                    contact= {food.dataOrder.phone}
+                    isYours={false}
+                    category={food.dataOrder.category} />
+                )
+            })
+            }
+            </div>
         </div>
     )
 }
-
 export default MyDonations
+
+
