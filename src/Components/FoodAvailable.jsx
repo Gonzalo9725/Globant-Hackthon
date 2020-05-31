bed57bd2c148d71787137c7795a8160e43529ec9
bdiniscia <bdiniscia@gmail.com>
2020-05-31 00:35:47 -0400

    Installing Firebase

--- src/Components/FoodAvailable.jsx
+++ src/Components/FoodAvailable.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './FoodAvailable.css'
 import FoodCard from './Widgets/FoodCard'
-import Button from './Widgets/Button'
 
 const FoodAvailable = () => {
     return (
